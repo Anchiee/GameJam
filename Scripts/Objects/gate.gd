c194c33fb1a83121305c1dec02514832d82e2eb7
NovaProot <ksawex17@gmail.com>
2026-04-21 17:22:55 +0200

    gate now opens and closes

--- Scripts/Objects/gate.gd
+++ Scripts/Objects/gate.gd
@@ -3,12 +3,14 @@ extends StaticBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @export var open_count: int = 1
 var boxes_on_buttons: int = 0
+@onready var texture_rect: TextureRect = $TextureRect
 
 func open() -> void:
 	boxes_on_buttons += 1
 	if boxes_on_buttons >= open_count:
 		collision_shape_2d.set_deferred("disabled", true)
 		collision_shape_2d.position.y += 1000
+		texture_rect.texture = load("uid://ic03nvtuxnho")
 
 
 func close() -> void:
@@ -16,3 +18,4 @@ func close() -> void:
 	if boxes_on_buttons <= open_count:
 		collision_shape_2d.set_deferred("disabled", false)
 		collision_shape_2d.position.y -= 1000
+		texture_rect.texture = load("uid://528gmdjvmilr")
