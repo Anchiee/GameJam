d6700ed6ffec42a1f329ee36ddf22dbb2314fa2e
NovaProot <ksawex17@gmail.com>
2026-04-21 12:44:14 +0200

    Error fix in levelman and cleanup

--- Scripts/Global/LevelManager.gd
+++ Scripts/Global/LevelManager.gd
@@ -1,16 +1,7 @@
 extends Node
 
 var current_level = 1
-const level_directory = "res://Scenes/World/"
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
+const level_directory = "res://Scenes/World/%s.tscn"
 
 func increase_room() -> void:
 	current_level += 1
@@ -18,4 +9,4 @@ func increase_room() -> void:
 func change_room() -> void:
 	increase_room()
 	if current_level <= 4:
-		get_tree().change_scene_to_file(level_directory + str(current_level) + ".tscn")
+		get_tree().change_scene_to_file.call_deferred(level_directory % current_level)
