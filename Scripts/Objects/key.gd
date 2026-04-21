c21b00c5b75448f189be4d133ce751a11f76ca4e
NovaProot <ksawex17@gmail.com>
2026-04-21 12:54:03 +0200

    adjusted key size

--- Scripts/Objects/key.gd
+++ Scripts/Objects/key.gd
@@ -1,10 +1,5 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _on_area_entered(_area: Area2D) -> void:
 	LevelManager.change_room()
