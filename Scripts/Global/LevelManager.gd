85de36fec0e44bff4d739ffac41a4dfaca180517
NovaProot <ksawex17@gmail.com>
2026-04-21 16:06:37 +0200

    you can now press esc to restart the room

--- Scripts/Global/LevelManager.gd
+++ Scripts/Global/LevelManager.gd
@@ -10,3 +10,7 @@ func change_room() -> void:
 	increase_room()
 	if current_level <= 8:
 		get_tree().change_scene_to_file.call_deferred(level_directory % current_level)
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
