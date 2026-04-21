de69c29e598217474757c8d923403bdd16521c08
NovaProot <ksawex17@gmail.com>
2026-04-21 16:26:25 +0200

    fixed bugged hole and box colisions

--- Scripts/Objects/hole.gd
+++ Scripts/Objects/hole.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
-@onready var static_body_2d: Node2D = $StaticBody2D
+@onready var static_body_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
 func _on_body_entered(body: Node2D) -> void:
 	print("Box!")
-	body.disable_collision()
-	if static_body_2d:
-		collision_shape_2d.queue_free()
+	if body:
+		body.disable_collision()
 	if collision_shape_2d:
-		collision_shape_2d.position.y += 1000
+		collision_shape_2d.set_deferred("disabled", true)
+	if static_body_2d:
+		static_body_2d.queue_free()
+	#if collision_shape_2d:
+		#collision_shape_2d.position.y += 1000
